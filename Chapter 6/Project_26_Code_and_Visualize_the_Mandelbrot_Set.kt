cd304fdcfb34d164893efd83cc9753cfb7530c3a
imfaisalgit <imfaisalLOQ@outlook.com>
2024-09-25 21:22:31 -0500

    Update Project_26_Code_and_Visualize_the_Mandelbrot_Set.kt
    Deleted canvas and attached the image directly to Pane()

--- Chapter 6/Project_26_Code_and_Visualize_the_Mandelbrot_Set.kt
+++ Chapter 6/Project_26_Code_and_Visualize_the_Mandelbrot_Set.kt
@@ -3,55 +3,59 @@
 
    This is the optimized version of the code that uses
    PixelWriter to change the color of the pixels of an image
-   in the memory before drawing the entire image in one go.
-
+   in the memory before displaying the image inside a pane.
+   
 */
 
 // import block
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.canvas.Canvas
-import javafx.scene.canvas.GraphicsContext
+import javafx.scene.image.ImageView
 import javafx.scene.image.PixelWriter
 import javafx.scene.image.WritableImage
 import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
 import javafx.stage.Stage
 
-// global parameters
+import kotlin.math.roundToInt
+
+// global parameters for the Mandelbrot set
+
 val xMin = -2.0
 val xMax = 1.0
 val yMin = -1.5
 val yMax = 1.5
-
-val xRange = xMax - xMin
-val yRange = yMax - yMin
-
-val canvasW = 600.0
-val canvasH = (canvasW/ xRange) * yRange
 val increment = 0.003
 val iterMax = 200
 
-// -----------------------------------------------------------------------
+// set the image parameters
+val xRange = xMax - xMin
+val yRange = yMax - yMin
+val imageWidth = 600
+val imageHeight = ((imageWidth / xRange) * yRange).roundToInt()
+
+/* ---------------------------------------------------------------------------------------- */
 
 // Application class for drawing the M-set
 class Mandelbrot : Application() {
     override fun start(stage: Stage) {
         val root = Pane()
-        val canvas = Canvas(canvasW, canvasH)
-        val gc = canvas.graphicsContext2D
-        root.children.add(canvas)
 
-        val scene = Scene(root, canvasW, canvasH)
+        val scene = Scene(root, imageWidth.toDouble(), imageHeight.toDouble())
         scene.fill = Color.WHITE
         stage.title = "Mandelbrot Set"
         stage.scene = scene
         stage.show()
 
-        // search for points in the M-set
-        // and draw them on the Canvas
-        drawMSet(gc)
+        // print global values
+        println()
+        println("image width = $imageWidth")
+        println("xRange = $xRange\tyRange = $yRange")
+        println("image height = $imageHeight")
+
+        // search for points in the M-set and display the set
+        drawMSet(root)
     }
 }
 
@@ -59,39 +63,42 @@ fun main() {
     Application.launch(Mandelbrot::class.java)
 }
 
-// -----------------------------------------------------------------------
+/* ---------------------------------------------------------------------------------------- */
 
-// function to iterate over the search space and draw non-members
-// using a grayscale and members as black points
+private fun drawMSet(root: Pane) {
+    println()
+    println("imageWidth = $imageWidth")
+    println("imageHeight = $imageHeight")
 
-private fun drawMSet(gc: GraphicsContext) {
-    // initialize a writable image with specified dimensions and
-    // access its pixelWrite method to directly manipulate its pixels
-    val image = WritableImage(canvasW.toInt(), canvasH.toInt())
-    val pixelWriter: PixelWriter = image.pixelWriter
-
+    val writableImage = WritableImage(imageWidth, imageHeight)
+    val pixelWriter: PixelWriter = writableImage.pixelWriter
 
     var y = yMin
     while (y <= yMax) {
         var x = xMin
-        while (x <= xMax ) {
+        while (x <= xMax) {
             val cval = getConvergence(x, y)
-            val speed = cval.toDouble() / iterMax
-            val factor = 1.0 -speed
-            val color = Color.color(factor, factor, factor)
 
-            val pixelX = (((x - xMin) / xRange) * canvasW).toInt()
-            val pixelY = (((y - yMin) / yRange) * canvasH).toInt()
+            val hue = (cval.toDouble() / iterMax) * 360.0
+            val brightness = if (cval < iterMax) 1.0 else 0.0
+            val color = Color.hsb(hue, 1.0, brightness)
+
+            val pixelX = (((x - xMin) / xRange) * (imageWidth - 1)).toInt()
+            val pixelY = (((y - yMin) / yRange) * (imageHeight -1)).toInt()
             pixelWriter.setColor(pixelX, pixelY, color)
+
             x += increment
         }
         y += increment
     }
-    // draw the entire image from memory to the canvas
-    gc.drawImage(image, 0.0, 0.0)
+    // Wrap WritableImage in ImageView (a node)
+    val imageView = ImageView(writableImage)
+    // Add ImageView to Pane
+    root.children.add(imageView)
+    println("----finished calculating----")
 }
 
-// -----------------------------------------------------------------------
+/* ---------------------------------------------------------------------------------------- */
 
 // function to check for membership in the M-set
 
@@ -99,9 +106,9 @@ private fun getConvergence (x: Double, y: Double): Int {
     var zx = 0.0
     var zy = 0.0
     for (i in 1..iterMax) {
-        val X = zx*zx - zy*zy + x
-        val Y = 2*zx*zy + y
-        if (X*X + Y*Y > 4.0) return i
+        val X = zx * zx - zy * zy + x
+        val Y = 2 * zx * zy + y
+        if (X * X + Y * Y > 4.0) return i
         zx = X
         zy = Y
     }
